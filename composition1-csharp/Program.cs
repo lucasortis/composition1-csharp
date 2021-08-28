60ba90b3742433e8270092a1a1b33cc1cd0a47c1
Lucas Ortis <lucasortis01@gmail.com>
2021-08-28 16:56:37 -0300

    adicionando .gitignore e iniciando projeto

---
+++ composition1-csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace composition1_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("How many contracts to this worker? ");
+        }
+    }
+}
